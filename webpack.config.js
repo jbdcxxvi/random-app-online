04038de1994406bfcc7a9f0d6a40d49c277144dd
jbdc0326 <160443063+jbdc0326@users.noreply.github.com>
2024-04-04 09:32:46 +0800

    changes to the application

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ const config = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: 'index.html',
+            template: './public/index.html',
         }),
 
         // Add your plugins here
